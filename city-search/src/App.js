1e088022fcd4685c4e1b6657b3a89066f3105d9d
Karanveer Singh <karansingh98123@gmail.com>
2019-06-05 20:11:59 -0400

    import CitySearch class

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CitySearch from './components/citySearch';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> City Search </h1>
+        <CitySearch />
       </header>
     </div>
   );
